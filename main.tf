0362fb160ee66747d19b6068a017e0de5fd0d206
Jeremy MARMOL <j.marmol@linkbynet.com>
2017-10-03 15:47:04 +0200

    Remove unwanted dash in domain-name-label

--- main.tf
+++ main.tf
@@ -99,7 +99,7 @@ resource "azurerm_public_ip" "vm" {
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.vm.name}"
   public_ip_address_allocation = "${var.public_ip_address_allocation}"
-  domain_name_label            = "${var.public_ip_dns}-${count.index}"
+  domain_name_label            = "${var.public_ip_dns}${count.index}"
 }
 
 resource "azurerm_network_security_group" "vm" {
@@ -136,6 +136,3 @@ resource "azurerm_network_interface" "vm" {
   }
 }
 
-
-  }
-}
