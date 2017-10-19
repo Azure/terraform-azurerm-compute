5426b9927713b885e50f0b2a8a107b83cc39934d
Jeremy MARMOL <j.marmol@linkbynet.com>
2017-10-19 10:10:53 +0200

    Add new mandatory parameters

--- main.tf
+++ main.tf
@@ -246,7 +246,7 @@ resource "azurerm_public_ip" "vm" {
 }
 
 resource "azurerm_network_security_group" "vm" {
-  name                = "${var.vm_hostname}nsg"
+  name                = "${var.vm_hostname}-nsg"
   location            = "${azurerm_resource_group.vm.location}"
   resource_group_name = "${azurerm_resource_group.vm.name}"
 
