a44798545a2f39cd830f3ad6a28a3c0b7b3eba3f
Junyi Yi <junyi@microsoft.com>
2017-12-07 15:15:34 -0800

    Fix tfstate issue caused by the breaking change of kitchen terraform.

--- test/integration/fixtures/outputs.tf
+++ test/integration/fixtures/outputs.tf
@@ -1,3 +1,8 @@
 output "test_target_public_dns" {
   value = "${module.linuxservers.public_ip_dns_name}"
 }
+
+output "terraform_state" {
+  description = "The path to the backend state file"
+  value       = "${path.cwd}/terraform.tfstate.d/${terraform.workspace}/terraform.tfstate"
+}
