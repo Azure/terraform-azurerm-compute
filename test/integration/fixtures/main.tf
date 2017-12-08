178de096498b8e002e29c0315cd06d7dc0a01e29
Junyi Yi <junyi@microsoft.com>
2017-12-07 22:10:11 -0800

    Fix tfstate issue caused by the breaking change of kitchen terraform. (#31)

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -1,11 +1,12 @@
 module "linuxservers" {
-  source              = "../../../"
-  location            = "${var.location}"
-  vm_os_simple        = "${var.vm_os_simple}"
-  public_ip_dns       = ["${var.public_ip_dns}"]
-  vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
-  ssh_key             = "${var.ssh_key}"
-  resource_group_name = "${var.resource_group_name}"
+  source                       = "../../../"
+  location                     = "${var.location}"
+  vm_os_simple                 = "${var.vm_os_simple}"
+  public_ip_address_allocation = "static"
+  public_ip_dns                = ["${var.public_ip_dns}"]
+  vnet_subnet_id               = "${module.network.vnet_subnets[0]}"
+  ssh_key                      = "${var.ssh_key}"
+  resource_group_name          = "${var.resource_group_name}"
 }
 
 module "network" {
