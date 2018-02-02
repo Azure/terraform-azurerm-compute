657a67883370346cf85c1830d5b73b707af2d48a
Richard Guthrie <richardguthrie@Richards-MacBook-Pro.local>
2018-02-02 15:22:30 -0800

    Fixing test to have random DNS name to avoid collissions and removing allow SSH

--- test/integration/fixtures/main.tf
+++ test/integration/fixtures/main.tf
@@ -3,15 +3,23 @@ module "linuxservers" {
   location                     = "${var.location}"
   vm_os_simple                 = "${var.vm_os_simple}"
   public_ip_address_allocation = "static"
-  public_ip_dns                = ["${var.public_ip_dns}"]
+  public_ip_dns                = ["${random_id.ip_dns.hex}"]
   vnet_subnet_id               = "${module.network.vnet_subnets[0]}"
   ssh_key                      = "${var.ssh_key}"
   resource_group_name          = "${var.resource_group_name}"
 }
 
 module "network" {
+  version             = "2.0.0"
   source              = "Azure/network/azurerm"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group_name}"
-  allow_ssh_traffic   = true
+}
+
+provider "random" {
+  version = "~> 1.0"
+}
+
+resource "random_id" "ip_dns" {
+  byte_length = 6
 }
