f66f021650d64aa68b2d3fb50a3d7425201d151a
Zhenchen Xiao <t-zhxia@microsoft.com>
2018-07-09 10:34:39 +0800

    style problem

--- terratest/compute/main.tf
+++ terratest/compute/main.tf
@@ -1,41 +1,41 @@
 provider "random" {
-    version = "~> 1.0"
+  version = "~> 1.0"
 }
 
 resource "random_id" "ip_dns" {
-    byte_length = 8
+  byte_length = 8
 }
 
 module "ubuntuservers" {
-    source                          = "Azure/compute/azurerm"
-    location                        = "${var.location}"
-    admin_username                  = "${var.admin_username}"
-    admin_password                  = "${var.admin_password}"
-    vm_os_simple                    = "${var.vm_os_simple_1}"
-    public_ip_dns                   = ["ubuntusimplevmips-${random_id.ip_dns.hex}"]
-    vnet_subnet_id                  = "${module.network.vnet_subnets[0]}"
-    ssh_key                         = "${var.ssh_key}"
-    resource_group_name             = "${var.resource_group_name}"
-    public_ip_address_allocation    = "static"
+  source                       = "Azure/compute/azurerm"
+  location                     = "${var.location}"
+  admin_username               = "${var.admin_username}"
+  admin_password               = "${var.admin_password}"
+  vm_os_simple                 = "${var.vm_os_simple_1}"
+  public_ip_dns                = ["ubuntusimplevmips-${random_id.ip_dns.hex}"]
+  vnet_subnet_id               = "${module.network.vnet_subnets[0]}"
+  ssh_key                      = "${var.ssh_key}"
+  resource_group_name          = "${var.resource_group_name}"
+  public_ip_address_allocation = "static"
 }
 
 module "debianservers" {
-    source                          = "Azure/compute/azurerm"
-    location                        = "${var.location}"
-    vm_hostname                     = "mylinvm"
-    admin_username                  = "${var.admin_username}"
-    admin_password                  = "${var.admin_password}"
-    vm_os_simple                    = "${var.vm_os_simple_2}"
-    public_ip_dns                   = ["debiansimplevmips-${random_id.ip_dns.hex}"] // change to a unique name per datacenter region
-    vnet_subnet_id                  = "${module.network.vnet_subnets[0]}"
-    ssh_key                         = "${var.ssh_key}"
-    resource_group_name             = "${var.resource_group_name}"
-    public_ip_address_allocation    = "static"
+  source                       = "Azure/compute/azurerm"
+  location                     = "${var.location}"
+  vm_hostname                  = "mylinvm"
+  admin_username               = "${var.admin_username}"
+  admin_password               = "${var.admin_password}"
+  vm_os_simple                 = "${var.vm_os_simple_2}"
+  public_ip_dns                = ["debiansimplevmips-${random_id.ip_dns.hex}"] // change to a unique name per datacenter region
+  vnet_subnet_id               = "${module.network.vnet_subnets[0]}"
+  ssh_key                      = "${var.ssh_key}"
+  resource_group_name          = "${var.resource_group_name}"
+  public_ip_address_allocation = "static"
 }
 
 module "network" {
-    source              = "Azure/network/azurerm"
-    version             = "2.0.0"
-    location            = "westus2"
-    resource_group_name = "${var.resource_group_name}"
+  source              = "Azure/network/azurerm"
+  version             = "2.0.0"
+  location            = "westus2"
+  resource_group_name = "${var.resource_group_name}"
 }
