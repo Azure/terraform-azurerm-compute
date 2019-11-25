1f6934542132ae79d0ce96c117ffeb7da7bff4bc
Szymon Matuszewski <22324341+szymon3@users.noreply.github.com>
2019-11-25 11:29:23 +0100

    Upgrade to TF 0.12 syntax

--- variables.tf
+++ variables.tf
@@ -97,7 +97,7 @@ variable "vm_os_version" {
 }
 
 variable "tags" {
-  type        = "map"
+  type        = map(any)
   description = "A map of the tags to use on the resources that are deployed with this module."
 
   default = {
@@ -131,7 +131,7 @@ variable "data_disk_size_gb" {
 }
 
 variable "data_disk" {
-  type        = "string"
+  type        = string
   description = "Set to true to add a datadisk."
   default     = "false"
 }
@@ -147,7 +147,7 @@ variable "boot_diagnostics_sa_type" {
 }
 
 variable "enable_accelerated_networking" {
-  type        = "string"
+  type        = string
   description = "(Optional) Enable accelerated networking on Network interface"
   default     = "false"
 }
