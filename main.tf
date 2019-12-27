97f2eccc9960473055bdd8293bc371eb94ed53e4
Michael Bawiec <michael.bawiec@gmail.com>
2019-12-27 16:58:48 +0900

    Move Security Group Rule to Dedicated Resource (#105)
    * Move security group rule to use azurerm_network_security_rule resource so that additional rules can be applied
    
    * apply terraform fmt
    
    * fix typo :)
    
    Co-authored-by: Yuping Wei <56525716+yupwei68@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -260,20 +260,22 @@ resource "azurerm_network_security_group" "vm" {
   location            = "${azurerm_resource_group.vm.location}"
   resource_group_name = "${azurerm_resource_group.vm.name}"
 
-  security_rule {
-    name                       = "allow_remote_${coalesce(var.remote_port,module.os.calculated_remote_port)}_in_all"
-    description                = "Allow remote protocol in from all locations"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "${coalesce(var.remote_port,module.os.calculated_remote_port)}"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-
   tags = "${var.tags}"
+}
+
+resource "azurerm_network_security_rule" "vm" {
+  name                        = "allow_remote_${coalesce(var.remote_port,module.os.calculated_remote_port)}_in_all"
+  description                 = "Allow remote protocol in from all locations"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "${coalesce(var.remote_port,module.os.calculated_remote_port)}"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = "${azurerm_resource_group.vm.name}"
+  network_security_group_name = "${azurerm_network_security_group.vm.name}"
 }
 
 resource "azurerm_network_interface" "vm" {
