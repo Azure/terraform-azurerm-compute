6c60af075a71eeac623b733a6c26503db2cffd8c
CrowderKroger <travis.crowder@kroger.com>
2020-09-17 13:57:31 -0400

    gh #145 adds support for vm display name outputs

--- test/fixture/outputs.tf
+++ test/fixture/outputs.tf
@@ -2,12 +2,24 @@ output "ubuntu_vm_public_name" {
   value = module.ubuntuservers.public_ip_dns_name
 }
 
+output "ubuntu_vm_names" {
+  value = module.ubuntuservers.vm_names
+}
+
 output "debian_vm_public_name" {
   value = module.debianservers.public_ip_dns_name
 }
 
+output "debian_vm_names" {
+  value = module.debianservers.vm_names
+}
+
 output "windows_vm_public_name" {
   value = module.windowsservers.public_ip_dns_name
+}
+
+output "windows_vm_names" {
+  value = module.windowsservers.vm_names
 }
 
 output "ubuntu_ip_address" {
