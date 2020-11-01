6467889e7453f975838c060a7838c7ecb7ad2fae
Ali Allomani <17993914+AliAllomani@users.noreply.github.com>
2020-11-02 00:41:58 +0100

    support os_profile_secrets

--- variables.tf
+++ variables.tf
@@ -208,3 +208,9 @@ variable "identity_ids" {
   type        = list(string)
   default     = []
 }
+
+variable "os_profile_secrets" {
+  description = "Specifies a list of certificates to be installed on the VM, each list item is a map with the keys source_vault_id, certificate_url, certificate_store (Windows Only)"
+  type = list(map(string))
+  default = []
+}
